620da6816a71f03b1a42f2e606ad5367da5b48e6
Emily Lampat <lampat26@bu.edu>
2023-11-30 16:34:16 -0500

    Trying to use timer begin and end to see about reaction timing.

--- app/src/main/java/com/example/scufflegame/MainActivity.java
+++ app/src/main/java/com/example/scufflegame/MainActivity.java
@@ -69,6 +69,9 @@ public class MainActivity extends Activity implements OnClickListener {
     // to that.
      Timer timerR = null;
      Timer timerL = null;
+     // Reaction time for the block
+    long begin = 0;
+    long end = 0;
 
      //Player health
      int healthA = 1;
@@ -94,32 +97,32 @@ public class MainActivity extends Activity implements OnClickListener {
         if (v.getId() == R.id.AattackR && BR == false) {
             attack('a','r');
         } else if (v.getId() == R.id.AblockR){
-           // block('a','r');
+           block('a','r');
         }
 
         if (v.getId() == R.id.AattackL && BL == false) {
             attack('a','l');
         } else if (v.getId() == R.id.AblockL){
-           // block('a','l');
+           block('a','l');
         }
 
         if (v.getId() == R.id.BattackR && AR == false) {
             attack('b','r');
         } else if (v.getId() == R.id.BblockR){
-           // block('b','r');
+           block('b','r');
         }
 
         if (v.getId() == R.id.BattackL && AL == false) {
             attack('b','l');
         } else if (v.getId() == R.id.BblockR){
-           // block('b','l');
+           block('b','l');
         }
 
 
 
     }
 
-    private char attack(char player, char side)
+    private long attack(char player, char side)
     {
         //activate attack indication animation
 
@@ -127,7 +130,8 @@ public class MainActivity extends Activity implements OnClickListener {
         if (side == 'r') {
             timerR.cancel();
             timerR = new Timer(); //start timer associated with right side attack
-            char attackout;
+            begin = System.nanoTime();
+
             if (player == 'a') {
                 AR = true;
             } else {
@@ -143,6 +147,8 @@ public class MainActivity extends Activity implements OnClickListener {
         } else {
             timerL.cancel();
             timerL = new Timer(); // start timer associated with left side attack
+            long begin = System.nanoTime();
+
             if (player == 'a') {
                 AL = true;
             } else {
@@ -155,36 +161,38 @@ public class MainActivity extends Activity implements OnClickListener {
             TimerTask punchL = new punchAnimation();
             timerPunchL.schedule(punchL, 400);
         }
+        return begin;
     }
 
 
     // cancel method of timer class
     private void block(char player, char side)
     {
+        end = System.nanoTime();
        // Take in the input of defense button
         if (side == 'r') {
-            if () {
-                if (side == 'a') {
+            if (begin - end < 400) {
+                if (player == 'a') {
                     healthA++; //negate damage done by attack from b on right side
                 }
-                else if (side =='b'){
+                else if (player =='b'){
                     healthB++; //negate damage done by attack from a on right side
                 }
             }
         }
         else if (side == 'l'){
-            if (timerL < 400){
-                if (side == 'a') {
+            if (begin - end < 400){
+                if (player == 'a') {
                     healthA++; //negate damage done by attack from b on left side
                 }
-                else if (side =='b'){
+                else if (player =='b'){
                     healthB++; //negate damage done by attack from a on left side
                 }
             }
         }
-       // Check if they pressed it in time for the defense
-
-       // reset global timer
+        begin = 0;
+        end = 0;
+       // reset timer
     }
 
 

5307389d037853176c6d9d621045c5efc22b6bf6
bernadelleb <152220000+bernadelleb@users.noreply.github.com>
2023-12-04 01:23:24 -0500

    adding instructions

--- app/src/main/java/com/example/scufflegame/HomeActivity.java
+++ app/src/main/java/com/example/scufflegame/HomeActivity.java
@@ -52,7 +52,15 @@ public class HomeActivity extends AppCompatActivity {
         builder.setTitle("How to Play");
 
         // Set up the instructions text. Consider using a string resource for this.
-        builder.setMessage("Here are the instructions on how to play the game...");
+        builder.setMessage("Attack: Press an attack button to hit your opponent.\n" + "\n" +
+                "Block: Use block buttons to defend against incoming attacks.\n" + "\n" +
+                "Lives: Each player starts with three lives. A successful attack reduces your opponent's life by one.\n"+ "\n" +
+                "The first player to drain all of the opponent's lives wins!\n" + "\n" +
+                "Tip: Watch for the lightning bolt signal for incoming attacks and time your blocks!\n" +
+                "\n" +
+                "Restart: After a game, choose \"Yes\" to play again or \"No\" to exit.\n" +
+                "\n" +
+                "Good luck!");
 
         // Add a button to dismiss the popup
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
